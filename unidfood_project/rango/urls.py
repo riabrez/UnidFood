2820a795756382af725cab6edd57bae01490a8a4
lewisawalker <lewis.a.walker@outlook.com>
2025-03-20 14:25:31 +0000

    Delete unidfood_project/rango directory

--- unidfood_project/rango/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rango import views
-
-app_name = 'rango'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-]
